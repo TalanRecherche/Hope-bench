d830a2889ad0ce3b015234b82c3184cee3e35e75
SBenRetD <steve.bellart@talan.com>
2024-07-12 18:38:28 +0200

    new try

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # update database
-alembic merge heads
+#alembic merge heads
 
-alembic revision --autogenerate -m "MIGRATION_NAME"
-alembic upgrade heads
+#alembic revision --autogenerate -m "MIGRATION_NAME"
+alembic upgrade 682c9e0b10af
 
 # add config to frontend part
 echo "window.hopeBenchConfig = {
