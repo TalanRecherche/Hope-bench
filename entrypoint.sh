5ad4359c4337464776dd29df12f247ec36ca0dc3
SBenRetD <steve.bellart@talan.com>
2024-08-30 14:09:03 +0200

    / en moins

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,21 @@ alembic upgrade head
 
 # Add config to frontend part
 echo "Configuring frontend..."
+
 echo "window.hope-benchConfig = {
   'KEYCLOAK_SERVER_URL': '$KEYCLOAK_SERVER_URL',
   'KEYCLOAK_REALM': '$KEYCLOAK_REALM',
   'KEYCLOAK_CLIENT_ID': '$KEYCLOAK_CLIENT_FRONT_ID',
   'BACKEND_BASE_URL': '/api/v1'
 }"
+
 echo "window.hopeBenchConfig = {
   'KEYCLOAK_SERVER_URL': '$KEYCLOAK_SERVER_URL',
   'KEYCLOAK_REALM': '$KEYCLOAK_REALM',
   'KEYCLOAK_CLIENT_ID': '$KEYCLOAK_CLIENT_FRONT_ID',
   'BACKEND_BASE_URL': '/api/v1'
 }" > /app/backend/src/front/env.js
+
 echo "window.hope-benchConfig = {
   'KEYCLOAK_SERVER_URL': '$KEYCLOAK_SERVER_URL',
   'KEYCLOAK_REALM': '$KEYCLOAK_REALM',
