2625bd98854999c8ad42280037bba948315befa4
Raphael Sturgis <raphael.sturgis@gmail.com>
2024-09-26 17:55:41 +0200

    Fixed issue with UUID

--- backend/src/models/Options.py
+++ backend/src/models/Options.py
@@ -1,33 +1,35 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 
 
 class Computer(BaseModel):
-    id_computer: str
+    id_computer: UUID
     model: str
 
 
 class Phone(BaseModel):
-    id_phone: str
+    id_phone: UUID
     model: str
 
 
 class Transport(BaseModel):
-    id_transport: str
+    id_transport: UUID
     transport_mode: str
 
 
 class ComputeProvider(BaseModel):
-    id_compute_provider: str
+    id_compute_provider: UUID
     name: str
     region: str
 
 
 class StorageProvider(BaseModel):
-    id_storage_provider: str
+    id_storage_provider: UUID
     name: str
     region: str
 
 
 class GPU(BaseModel):
-    id: str
+    id: UUID
     name: str
